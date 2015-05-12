e7f3b24831ee72712a52fa49e5a4af0fd892d93c
Robin Houdmeyers <houdmeyers@gmail.com>
2015-05-13 00:16:02 +0200

    style bar page

---
+++ app/helpers/is-today.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+
+export function isToday(params) {
+  var weekday = params[0];
+
+  var days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+  if (days.indexOf(weekday) === new Date().getDay()) {
+    return 'today'
+  } else {
+    return null;
+  }
+}
+
+export default Ember.HTMLBars.makeBoundHelper(isToday);
